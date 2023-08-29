41f00419b8ad062b6b514abe1419bfb5603a2548
Bomowo <oeccc25@yahoo.com>
2023-08-29 21:15:23 +0300

    Add die faces on dice

--- src/Die.js
+++ src/Die.js
@@ -6,11 +6,10 @@ export default function Die(props) {
     }
     return (
         <div 
-            className="die-face" 
+            className={"die-face"+ " " + "die" + props.value} 
             style={styles}
             onClick={props.holdDice}
         >
-            <h2 className="die-num">{props.value}</h2>
         </div>
     )
 }
